91124cd730556ef6b6c6297cb1fae0f7d334468c
Anna Kuzmenko <ask147g@gmail.com>
2026-02-05 11:03:52 +0300

    refactor(Device): moved realization of template methods to IDevice.icpp

--- device/include/IDevice.h
+++ device/include/IDevice.h
@@ -43,120 +43,14 @@ namespace Device {
         void SetIsThreshold(bool val) {bThreshold = val;};
         bool GetIsThreshold() {return bThreshold;};
 
-        template<typename T> std::vector<double> NormalizeWaveform(std::vector<T> waveform) {
-            auto valMax = std::max_element(waveform.begin(), waveform.end());
-            auto valMin = std::min_element(waveform.begin(), waveform.end());
-
-            if (std::abs(*valMax) < std::abs(*valMin)) valMax = valMin;
-            std::vector<double> nWaveform = {};
-            for (auto &val : waveform) nWaveform.push_back(val/(*valMax));
-
-            return nWaveform;
-        };
-
-        template<typename T> std::vector<double> CCF(std::vector<T> w1, std::vector<T> w2) {
-            TSpectrumTransform* tspct = new TSpectrumTransform(w1.size());
-            tspct->SetTransformType(TSpectrumTransform::kTransformFourier, 0);
-            tspct->SetDirection(TSpectrumTransform::kTransformForward);
-            std::vector<T> lvalue;
-            std::vector<T> rvalue;
-            lvalue.resize(2*w1.size());
-            rvalue.resize(2*w1.size());
-            tspct->Transform(w1.data(), lvalue.data());
-            tspct->Transform(w2.data(), rvalue.data());
-            for (int i = 0; i < lvalue.size(); i++) lvalue[i] *= rvalue[i];
-            rvalue.clear();
-            rvalue.resize(w1.size());
-            tspct->SetDirection(TSpectrumTransform::kTransformInverse);
-            tspct->Transform(lvalue.data(), rvalue.data());
-            for (int i = 0; i < rvalue.size(); i++) rvalue[i] /= lvalue[i];
-            return rvalue;
-        };
-
-        template<typename T> double TemplateCalculateAmplitude(std::vector<T> eventWaveform, double baseline) {
-            Global::Parameters usedParameters = GetParser()->GetUsedParameters();
-
-            double amplitude = 0;
-            TF1* parabola = new TF1("parabola", "pol2", usedParameters.signalRange.value().first, usedParameters.signalRange.value().second);
-            TGraph* gr = new TGraph();
-            for (size_t i = 0; i < eventWaveform.size(); i++) {
-                gr->SetPoint(i, i, eventWaveform[i]);
-            }
-            gr->Fit("parabola", "QR");
-            double x = -parabola->GetParameter(1)/(2*parabola->GetParameter(2));
-            amplitude = parabola->GetParameter(2)*x*x+parabola->GetParameter(1)*x+parabola->GetParameter(0)-baseline;
-            double factor = 1;
-            double shift = 0;
-            if (usedParameters.factorAmplitude.has_value()) factor = usedParameters.factorAmplitude.value();
-            if (usedParameters.shiftAmplitude.has_value()) shift = usedParameters.shiftAmplitude.value();
-            amplitude = amplitude * factor + shift;
-            return amplitude;
-        };
-
-        template<typename T> double TemplateCalculateBaseline(std::vector<T> eventWaveform) {
-            Global::Parameters usedParameters = GetParser()->GetUsedParameters();
-            int min = usedParameters.baselineLimits.value().first;
-            int max = usedParameters.baselineLimits.value().second;
-
-            double ss = 0;
-            for (size_t i = 0; i < size(eventWaveform); i++) {
-                if ((i >= min) && (i <= max)) {
-                    ss += eventWaveform[i];
-                }
-            }
-            return ss/(max-min+1.);
-        };
-
-        template<typename T> double TemplateCalculateCharge(std::vector<T> eventWaveform, double baseline) {
-            Global::Parameters usedParameters = GetParser()->GetUsedParameters();
-            int min = usedParameters.signalRange.value().first;
-            int max = usedParameters.signalRange.value().second;
-            double charge = 0;
-            int counter = 0;
-            for (double waveform : eventWaveform) {
-                if ((counter >= min) && (counter <= max)) charge += waveform - baseline;
-                counter++;
-            }
-            double factor = 1;
-            double shift = 0;
-            if (usedParameters.factorCharge.has_value()) factor = usedParameters.factorCharge.value();
-            if (usedParameters.shiftCharge.has_value()) shift = usedParameters.shiftCharge.value();
-            return charge * factor + shift;
-        };
-
-        template<typename T> bool ThresholdSignalFilter(std::vector<T> eventWaveform, double baseline) {
-            Global::Parameters usedParameters = GetParser()->GetUsedParameters();
-            double amplitude = TemplateCalculateAmplitude(eventWaveform, baseline);
-
-            if ((!usedParameters.signal.has_value()) || (!usedParameters.threshold.has_value())) {
-                if (!usedParameters.signal.has_value()) throw std::runtime_error("Signal form is not defined");
-                else throw std::runtime_error("Threshold is not defined");
-            }
-
-            if ((usedParameters.signal.value() == "up") && (amplitude > usedParameters.threshold.value())) return true;
-            if ((usedParameters.signal.value() == "down") && (amplitude < usedParameters.threshold.value())) return true;
-
-            return false;
-        };
-
-        template<typename T1, typename T2> bool CCFSignalFilter(std::vector<T1> eventWaveform, std::vector<T2> averageWaveform) {
-            std::vector<double> fEventWaveformN = NormalizeWaveform(averageWaveform);
-            double r = MaxCCF(CCF(fEventWaveformN, NormalizeWaveform(eventWaveform)))/MaxCCF(CCF(fEventWaveformN, fEventWaveformN));
-            Global::Parameters usedParameters = GetParser()->GetUsedParameters();
-
-            if (!usedParameters.cut.has_value()) throw std::runtime_error("Cut is not defined");
-            
-            if (r*100 < usedParameters.cut.value()) return false;
-            return true;
-        };
-
-        template<typename T1, typename T2> bool SignalFilter(std::vector<T1> eventWaveform, std::vector<T2> averageWaveform, double baseline) {
-            if ((!GetIsCut()) && (!GetIsThreshold())) return true;
-            if ((GetIsCut()) && (!GetIsThreshold()) && (CCFSignalFilter(eventWaveform, averageWaveform))) return true;
-            if ((!GetIsCut()) && (GetIsThreshold()) && (ThresholdSignalFilter(eventWaveform, baseline))) return true;
-            if ((GetIsCut()) && (GetIsThreshold()) && (CCFSignalFilter(eventWaveform, averageWaveform)) && (ThresholdSignalFilter(eventWaveform, baseline))) return true;
-            return false;
-        };
+        template<typename T> std::vector<double> NormalizeWaveform(std::vector<T> waveform);
+        template<typename T> std::vector<double> CCF(std::vector<T> w1, std::vector<T> w2);
+        template<typename T> double TemplateCalculateAmplitude(std::vector<T> eventWaveform, double baseline);
+        template<typename T> double TemplateCalculateBaseline(std::vector<T> eventWaveform);
+        template<typename T> double TemplateCalculateCharge(std::vector<T> eventWaveform, double baseline);
+        template<typename T> bool ThresholdSignalFilter(std::vector<T> eventWaveform, double baseline);
+        template<typename T1, typename T2> bool CCFSignalFilter(std::vector<T1> eventWaveform, std::vector<T2> averageWaveform);
+        template<typename T1, typename T2> bool SignalFilter(std::vector<T1> eventWaveform, std::vector<T2> averageWaveform, double baseline);
 
         double MaxCCF(std::vector<double> ccfvector);
     private:
@@ -173,4 +67,6 @@ namespace Device {
         bool bDebug = false;
         bool bThreshold = false;
     };
+
+    #include "IDevice.icpp"
 }
